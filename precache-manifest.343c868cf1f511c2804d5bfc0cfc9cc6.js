d9fed2599a468dc44676bb0dbed589079834be5e
Leão <mtdevjf@gmail.com>
2022-10-14 19:50:45 -0300

    update

---
+++ precache-manifest.343c868cf1f511c2804d5bfc0cfc9cc6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd37b0fef2de9fab2224",
+    "url": "/static/css/main.90207e90.chunk.css"
+  },
+  {
+    "revision": "cd37b0fef2de9fab2224",
+    "url": "/static/js/main.cd37b0fe.chunk.js"
+  },
+  {
+    "revision": "d38e7ded8baeb2f57d87",
+    "url": "/static/js/1.d38e7ded.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9a5d056c560c64850be1994874613a7f",
+    "url": "/static/media/cv.9a5d056c.pdf"
+  },
+  {
+    "revision": "f7b3f22cf1a878f3a8cd18e551e38b17",
+    "url": "/static/media/me.f7b3f22c.png"
+  },
+  {
+    "revision": "064a202c43e529e849167cd067961994",
+    "url": "/static/media/me-about.064a202c.png"
+  },
+  {
+    "revision": "66423b8c3124cb942845372746b30564",
+    "url": "/static/media/Pokedex.66423b8c.jpg"
+  },
+  {
+    "revision": "a6e3ea1fe8cd7ff57d43a65424bb17e9",
+    "url": "/static/media/P.portifolio.a6e3ea1f.jpg"
+  },
+  {
+    "revision": "b70aaaae4f00f3d11f0c254ebf630c69",
+    "url": "/static/media/MSocial.b70aaaae.jpg"
+  },
+  {
+    "revision": "cd2bc540cbdfffee0d7ff26d49bc6b30",
+    "url": "/index.html"
+  }
+];
